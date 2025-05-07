cf6f60f4573fb62d8cf15a3980235e9a26fe0d6e
eomer <eomer@VIKINGPC>
2025-05-07 13:44:55 +1000

    Add project files.

---
+++ VikingFileOrganiser/MainForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace FileOrganizerApp
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.txtSelectedFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvFiles = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnOrganize = new System.Windows.Forms.Button();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSelectFolder.Location = new System.Drawing.Point(24, 22);
+            this.btnSelectFolder.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(200, 42);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // txtSelectedFolder
+            // 
+            this.txtSelectedFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSelectedFolder.Location = new System.Drawing.Point(236, 26);
+            this.txtSelectedFolder.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtSelectedFolder.Name = "txtSelectedFolder";
+            this.txtSelectedFolder.ReadOnly = true;
+            this.txtSelectedFolder.Size = new System.Drawing.Size(1116, 29);
+            this.txtSelectedFolder.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 92);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Files in folder:";
+            // 
+            // lvFiles
+            // 
+            this.lvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lvFiles.FullRowSelect = true;
+            this.lvFiles.GridLines = true;
+            this.lvFiles.HideSelection = false;
+            this.lvFiles.Location = new System.Drawing.Point(24, 122);
+            this.lvFiles.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.lvFiles.Name = "lvFiles";
+            this.lvFiles.Size = new System.Drawing.Size(1328, 274);
+            this.lvFiles.TabIndex = 3;
+            this.lvFiles.UseCompatibleStateImageBehavior = false;
+            this.lvFiles.View = System.Windows.Forms.View.Details;
+            this.lvFiles.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.lvFiles_AfterLabelEdit);
+            this.lvFiles.SelectedIndexChanged += new System.EventHandler(this.lvFiles_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Files";
+            this.columnHeader1.Width = 300;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Folder Name";
+            this.columnHeader2.Width = 300;
+            // 
+            // btnOrganize
+            // 
+            this.btnOrganize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOrganize.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOrganize.Location = new System.Drawing.Point(1156, 410);
+            this.btnOrganize.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnOrganize.Name = "btnOrganize";
+            this.btnOrganize.Size = new System.Drawing.Size(200, 42);
+            this.btnOrganize.TabIndex = 4;
+            this.btnOrganize.Text = "Organize Files";
+            this.btnOrganize.UseVisualStyleBackColor = true;
+            this.btnOrganize.Click += new System.EventHandler(this.btnOrganize_Click);
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Location = new System.Drawing.Point(24, 506);
+            this.txtConsole.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ReadOnly = true;
+            this.txtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtConsole.Size = new System.Drawing.Size(1328, 274);
+            this.txtConsole.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 476);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Console:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 807);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 24);
+            this.label3.TabIndex = 7;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.ForeColor = System.Drawing.Color.Red;
+            this.progressBar.Location = new System.Drawing.Point(138, 799);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1218, 42);
+            this.progressBar.Step = 5;
+            this.progressBar.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.BackgroundImage = global::VikingFileOrganiser.Properties.Resources.embers;
+            this.ClientSize = new System.Drawing.Size(1380, 864);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtConsole);
+            this.Controls.Add(this.btnOrganize);
+            this.Controls.Add(this.lvFiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSelectedFolder);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Gold;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MinimumSize = new System.Drawing.Size(1184, 705);
+            this.Name = "MainForm";
+            this.Text = "Viking File Organizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.TextBox txtSelectedFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lvFiles;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btnOrganize;
+        private System.Windows.Forms.TextBox txtConsole;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
---
+++ VikingFileOrganiser/MainForm.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic; // Add this namespace for List<T>
+using System.Linq; // Add this namespace for LINQ methods like Cast
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FileOrganizerApp
+{
+    public partial class MainForm : Form
+    {
+        private string selectedFolderPath;
+        private BackgroundWorker backgroundWorker;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeBackgroundWorker();
+        }
+
+        private void InitializeBackgroundWorker()
+        {
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFolderPath = folderDialog.SelectedPath;
+                    txtSelectedFolder.Text = selectedFolderPath;
+                    LoadFolderContents();
+                }
+            }
+        }
+
+        private void LoadFolderContents()
+        {
+            lvFiles.Items.Clear();
+
+            if (!Directory.Exists(selectedFolderPath))
+            {
+                MessageBox.Show("Selected folder does not exist!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var files = Directory.GetFiles(selectedFolderPath);
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                var item = new ListViewItem(fileName);
+                item.SubItems.Add(fileName); // Second column for editable name
+                item.Tag = file; // Store full path in Tag
+                lvFiles.Items.Add(item);
+            }
+        }
+
+        private void btnOrganize_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedFolderPath))
+            {
+                MessageBox.Show("Please select a folder first!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (lvFiles.Items.Count == 0)
+            {
+                MessageBox.Show("No files to organize!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            // Update any renamed files first
+            foreach (ListViewItem item in lvFiles.Items)
+            {
+                string originalPath = (string)item.Tag;
+                string newName = item.SubItems[1].Text;
+                string originalName = Path.GetFileName(originalPath);
+
+                if (newName != originalName)
+                {
+                    try
+                    {
+                        string newPath = Path.Combine(selectedFolderPath, newName);
+                        File.Move(originalPath, newPath);
+                        item.Tag = newPath; // Update the stored path
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error renaming {originalName} to {newName}: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            txtConsole.Clear();
+            progressBar.Value = 0;
+            btnOrganize.Enabled = false;
+            btnSelectFolder.Enabled = false;
+
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int totalFiles = lvFiles.Items.Count;
+            int processedFiles = 0;
+
+            // Retrieve the items from the ListView
+            var items = (List<ListViewItem>)lvFiles.Invoke(new Func<List<ListViewItem>>(() => lvFiles.Items.Cast<ListViewItem>().ToList()));
+
+            foreach (var item in items) // Add a foreach loop to iterate over the items
+            {
+                string filePath = (string)item.Tag;
+                string fileName = Path.GetFileName(filePath);
+                string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+
+                // Skip the organize executable/script itself
+                if (fileNameWithoutExtension.Equals("organize", StringComparison.OrdinalIgnoreCase))
+                {
+                    processedFiles++;
+                    continue;
+                }
+
+                try
+                {
+                    string folderPath = Path.Combine(selectedFolderPath, fileNameWithoutExtension);
+
+                    // Report progress before creating folder
+                    backgroundWorker.ReportProgress((processedFiles * 100) / totalFiles, $"Creating folder: {fileNameWithoutExtension}");
+
+                    Directory.CreateDirectory(folderPath);
+
+                    // Report progress before moving
+                    backgroundWorker.ReportProgress((processedFiles * 100) / totalFiles, $"Moving {fileName} to {fileNameWithoutExtension}");
+
+                    string newFilePath = Path.Combine(folderPath, fileName);
+                    File.Move(filePath, newFilePath);
+
+                    processedFiles++;
+                    backgroundWorker.ReportProgress((processedFiles * 100) / totalFiles, $"Success: {fileName} moved to {fileNameWithoutExtension}");
+                }
+                catch (Exception ex)
+                {
+                    backgroundWorker.ReportProgress((processedFiles * 100) / totalFiles, $"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)        {
+            progressBar.Value = e.ProgressPercentage;
+            if (e.UserState != null)
+            {
+                txtConsole.AppendText(e.UserState.ToString() + Environment.NewLine);
+            }
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar.Value = 100;
+            btnOrganize.Enabled = true;
+            btnSelectFolder.Enabled = true;
+            LoadFolderContents(); // Refresh the file list
+            txtConsole.AppendText("Organization complete!" + Environment.NewLine);
+        }
+
+        private void lvFiles_AfterLabelEdit(object sender, LabelEditEventArgs e)
+        {
+            if (e.Label != null && e.Label.Length > 0)
+            {
+                lvFiles.Items[e.Item].SubItems[1].Text = e.Label;
+            }
+            else
+            {
+                e.CancelEdit = true;
+            }
+        }
+
+        private void lvFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
